852e35ede8bd66f5732c8689c4cf7f087fd3f842
Mukesh-edits <mukeshkumar.b2023@vitstudent.ac.in>
2026-05-25 17:12:38 +0530

    fix: cors and mongo uri for deployment

--- server/server.js
+++ server/server.js
@@ -2,32 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 const authRoutes = require('./routes/authRoutes');
 const repoRoutes = require('./routes/repoRoutes');
-
 app.use('/api/auth', authRoutes);
 app.use('/api/repo', repoRoutes);
 
-// Test route
 app.get('/', (req, res) => {
   res.json({ message: 'DevLens API is running!' });
 });
 
-// Database connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected successfully');
